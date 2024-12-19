0b604ece0dfff081aef9711ffcdff4e788da9442
mrdafian47 <mohamad.rizki.dafianto@gmail.com>
2024-12-19 10:33:34 +0700

    example: show font app

---
+++ src/main/java/com/github/dafian/app/example/font/FontExampleApp.java
@@ -0,0 +1,103 @@
+package com.github.dafian.app.example.font;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.VPos;
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.*;
+import javafx.stage.Stage;
+
+import java.util.List;
+
+public class FontExampleApp extends Application {
+
+    private static final double WIDTH = 600;
+    private static final double HEIGHT = 900;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        String fontFamily = "Arial";
+        double fontSize = 13;
+        FontWeight fontWeight = FontWeight.BOLD;
+        FontPosture fontPosture = FontPosture.ITALIC;
+
+        Font font1 = Font.font(fontFamily);
+        Font font2 = Font.font(fontSize);
+        Font font3 = Font.font(fontFamily, fontSize);
+        Font font4 = Font.font(fontFamily, fontWeight, fontSize);
+        Font font5 = Font.font(fontFamily, fontPosture, fontSize);
+        Font font6 = Font.font(fontFamily, fontWeight, fontPosture, fontSize);
+
+        Font arialFont = Font.font("Arial");
+        Font arialFontBold36 = Font.font("Arial", FontWeight.BOLD, 36);
+
+        Text textArial = createText("Text in Arial", arialFont, 0, 30);
+        Text textArialBold36 = createText("Text in Arial Bold 36", arialFontBold36, 0, 70);
+
+        Font robotoFont = Font.font("Roboto");
+        Font robotoFontBold36 = Font.font("Roboto", FontWeight.BOLD, 36);
+
+        Text textRoboto = createText("Text in Roboto", robotoFont, 0, 100);
+        Text textRobotoBold36 = createText("Text in Roboto Bold 36", robotoFontBold36, 0, 140);
+
+        Font courierNewFont = Font.font("Courier New");
+        Font courierNewFontBold36 = Font.font("Courier New", FontWeight.BOLD, 36);
+
+        Text textCourierNew = createText("Text in Courier New", courierNewFont, 0, 180);
+        Text textCourierNewBold36 = createText("Text in Courier New Bold 36", courierNewFontBold36, 0, 220);
+
+        Pane pane = new Pane();
+        pane.getChildren().add(textArial);
+        pane.getChildren().add(textArialBold36);
+        pane.getChildren().add(textRoboto);
+        pane.getChildren().add(textRobotoBold36);
+        pane.getChildren().add(textCourierNew);
+        pane.getChildren().add(textCourierNewBold36);
+
+        HBox hBox = new HBox(10, pane);
+        hBox.setPadding(new Insets(10));
+
+        Scene scene = new Scene(hBox, WIDTH, HEIGHT);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        List<String> fontFamilies = Font.getFamilies();
+        List<String> fontNames = Font.getFontNames();
+
+        System.out.println("=====================");
+        System.out.println("Font families: " + fontFamilies.size());
+        System.out.println("=====================");
+        printList(fontFamilies);
+        System.out.println("=====================");
+        System.out.println("Font names: " + fontNames.size());
+        System.out.println("=====================");
+        printList(fontNames);
+    }
+
+    private void printList(List<String> list) {
+        for (String item : list) {
+            System.out.println(item);
+        }
+    }
+
+    private Text createText(String textToDisplay, Font font, double x, double y) {
+        Text text = new Text(textToDisplay);
+        text.setFontSmoothingType(FontSmoothingType.GRAY);
+        text.setFont(font);
+        text.setFill(Color.DARKMAGENTA);
+        text.setTextOrigin(VPos.BASELINE);
+
+        text.setX(x);
+        text.setY(y);
+
+        return text;
+    }
+}
